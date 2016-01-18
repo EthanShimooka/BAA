f3a310ab3bcef9a73a5d29297cf1e8535b7ab033
blakewilliams1 <blauwill@Lighting.adgpm.soe.ucsc.edu>
2016-01-18 15:32:42 -0800

    Updated project settings to include the SDL library
    Make sure to download SDL at:
    https://www.libsdl.org/release/SDL2-devel-2.0.4-VC.zip and extract in
    into the SquadIO project folder. If you have any doubts, check the
    include paths in the project properties pages.

---
+++ SquadIO/RenderManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef SDL2DRENDERMANAGER_H_INCLUDED
+#define SDL2DRENDERMANAGER_H_INCLUDED
+
+#include "SDL.h"
+
+
+class RenderManager
+{
+public:
+	RenderManager();
+	~RenderManager();
+};
+
+#endif //SDL2DRENDERMANAGER_H_INCLUDED
