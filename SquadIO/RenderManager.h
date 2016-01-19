2c4272c24de7b15b8335a34722a940c3b4342578
blakewilliams1 <blauwill@Lighting.adgpm.soe.ucsc.edu>
2016-01-18 17:19:58 -0800

    Implemented RenderManager header files
    The rendermanager skeleton is done, now to flesh it out. IMPORTANT!!!
    You must copy visual studio requires that we copy SDL2.lib and
    SDL2main.lib to the same folder that the SquadIO .h and .cpp files are
    in.
    Still need to implement the tinyurl library. will provide instructions
    on it when finished.

--- SquadIO/RenderManager.h
+++ SquadIO/RenderManager.h
@@ -1,14 +1,88 @@
 #pragma once
 #ifndef SDL2DRENDERMANAGER_H_INCLUDED
 #define SDL2DRENDERMANAGER_H_INCLUDED
+// TODO: page 135, add the first two includes: 2DRenderManager.h and ResourceManager.h they are made by us
+#include "EngineObject.h"
 
 #include "SDL.h"
+#include <SDL_image.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <Windows.h>
+#include <math.h>
+#include <list>
+// TODO: import the tinyxml library
+//#include "TimyXML.h"
 
-
-class RenderManager
-{
+class RenderResource{// : public cResource{ TODO: add this inheritance in when resource manager is done
+private:
+protected:
 public:
+	SDL_Surface *mSurface;
+
+	~RenderResource();
+	void load();
+	void unload();
+	RenderResource();
+};
+
+class SDLRenderObject : public EngineObject{
+private:
+protected:
+public:
+	RenderResource *renderResource;
+	SDL_Rect renderRect;
+	float posX;
+	float posY;
+	bool visible;
+	SDL_Color colorKey;
+	bool colorKeyEnabled;
+
+	SDLRenderObject();
+	void setResourceObject(RenderResource *source);
+	virtual void update(){};
+};
+
+class SpriteObject : public SDLRenderObject{
+private:
+protected:
+	DWORD timeLastFrame;
+
+public:
+	unsigned int totalFrames;
+	unsigned int framesPerRow;
+	unsigned int framesPerColumn;
+	unsigned int currentFrame;
+	unsigned int startFrame;
+	unsigned int frameWidth;
+	unsigned int frameHeight;
+
+	void update();
+	void play();
+	void stop();
+	void setFrameRect(unsigned int frameNumber);
+	SpriteObject();
+};
+
+class RenderManager{// : public c2DRenderManager
+private:
+protected:
 	RenderManager();
+	static RenderManager renderManager;
+public:
+	static RenderManager* getRenderManager();
+	SDL_Surface* renderWindow;
+	std::stringstream videoInfo;
+	bool init(unsigned int Width = 800, unsigned int Height = 600, bool fullScreen = false, char* WindowTitle = 0);
+	void free();
+	bool update();
+	void toggleFullScreen();
+	//cResource* loadResourceFromXML(TiXmlElement* element);
+	void renderAllObjects();
+	std::list<SDLRenderObject> renderObjects;
 	~RenderManager();
 };
 
