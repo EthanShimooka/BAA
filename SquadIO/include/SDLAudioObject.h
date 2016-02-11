08e5f92a3bb570c9d95cbc9d54390e77005d21a2
UVERkill <vincentchen12@mittymonarch.com>
2016-02-10 23:46:09 -0800

    created SDLAudioObject
    engine object for audio, holds AudioResource pointer and other necessary
    fields

---
+++ SquadIO/include/SDLAudioObject.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef SDLAUDIOOBJECT_H_INCLUDED
+#define SDLAUDIOOBJECT_H_INCLUDED
+
+#ifdef SQUADIO_EXPORTS
+#define SQUADIO_API __declspec(dllexport) 
+#else
+#define SQUADIO_API __declspec(dllimport) 
+#endif
+
+#include "EngineObject.h"
+#include "AudioResource.h"
+#include "sdl2\SDL.h"
+#include "sdl2\SDL_mixer.h"
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <Windows.h>
+#include <math.h>
+#include <list>
+
+/*
+* SDLAudioObject is the "renderable" object that take a pointer to an audioResource (gameResource).
+*/
+class SDLAudioObject : public EngineObject{
+private:
+protected:
+public:
+	AudioResource* audioResource;
+
+	// true for bgm, false for effect
+	bool isBgm;
+
+	// constructor
+	SQUADIO_API SDLAudioObject();
+	/// Set a resource object from a Audio Resource onto a SDL surface 
+	SQUADIO_API void setResourceObject(gameResource* source);
+}
+
+#endif // SDLAUDIOOBJECT_H_INCLUDED
