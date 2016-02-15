68a5c81ee0993e35cfc33f03fe34c549798ddb8b
Ricardo-Murillo <aydonspek@gmail.com>
2016-02-15 01:44:59 -0800

    Preliminary Sprites

--- Test/test.cpp
+++ Test/test.cpp
@@ -59,18 +59,40 @@ int _tmain(int argc, _TCHAR* argv[]){
 	Square* player2 = new Square(200, 200, 2);
 	player2->obj = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 12, player2->x, player2->y);
 
-
+	struct point{
+		int x = 0;
+		int y = 0;
+	}center;
+	//center = {};
+	Square* leg = new Square(14+center.x, 60+center.y, 3);
+	leg->obj = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 103, leg->x, leg->y);
+	leg->obj->anchor = { 42 / float(leg->obj->renderRect.w), 2 / float(leg->obj->renderRect.h) };
+	Square* armor = new Square(0+center.x, 0+center.y, 4);
+	armor->obj = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 101, armor->x, armor->y);
+	armor->obj->anchor = {0,0};
+	Square* arm = new Square(31+center.x, 43+center.y, 5);
+	arm->obj = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 102, arm->x, arm->y);
+	arm->obj->anchor = { 14 / float(arm->obj->renderRect.w), 3 / float(arm->obj->renderRect.h) };
 	/////////////////////////////////////////////////////
 	/*              * * * GAME LOOP * * *              */
 	/////////////////////////////////////////////////////
 	bool gameloop = true;
 
+	int var = 0;
 	while (gameloop) {
+		var += 1;
 		//NetworkManager::sInstance->ProcessIncomingPackets();
 		listen->getInput();
 
-		player2->x += listen->input_x;
-		player2->y += listen->input_y;
+		//arm->obj->rotation = var * 2;
+		center.x += listen->input_x;
+		center.y += listen->input_y;
+		armor->obj->posX = 0 + center.x;
+		armor->obj->posY = 0 + center.y;
+		leg->obj->posX = 14 + armor->obj->posX;
+		leg->obj->posY = 60 + armor->obj->posY;
+		arm->obj->posX = 31 + armor->obj->posX;
+		arm->obj->posY = 43 + armor->obj->posY;
 
 		//cout << player1->obj->posX << "," << player2->obj->posX<< endl;
 
