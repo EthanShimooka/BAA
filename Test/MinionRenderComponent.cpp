be9c03ba1aea02404fb7585b70d150fc9d57c37b
PouryaSohbati <sohbati.pourya@gmail.com>
2016-02-27 19:14:54 -0800

    finished out minions and added setPos to GameObject

--- Test/MinionRenderComponent.cpp
+++ Test/MinionRenderComponent.cpp
@@ -4,7 +4,7 @@
 MinionRenderComponent::MinionRenderComponent(){
 	SceneManager* sceneMan = SceneManager::GetSceneManager();
 
-	objRef = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 12, 100, 0);
+	objRef = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 12, 0, 0);
 }
 
 
@@ -13,5 +13,5 @@ MinionRenderComponent::~MinionRenderComponent()
 }
 
 void MinionRenderComponent::Update(){
-
+	RenderComponent::Update();
 }
