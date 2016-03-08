bbfcbf7419c606912f45c67c0f5dcbea3d961799
Ricardo-Murillo <aydonspek@gmail.com>
2016-03-07 16:59:05 -0800

    Readded commented out test inputs

--- Test/test.cpp
+++ Test/test.cpp
@@ -225,11 +225,11 @@ int _tmain(int argc, _TCHAR* argv[]){
 		//arm->posY = 43 + armor->posY;
 	
 	//audioMan->playByName("bgmfostershome.ogg");
-	//int mousecounter = 5;
+	int mousecounter = 5;
 	while (gameloop) {
 
 		////test inputs, delete if you want//
-		/*
+		
 		if (input->isKeyDown(KEY_Q)){
 			if (renderMan->cameraPoint.z < -5){
 				renderMan->cameraPoint.z += 1;
@@ -248,7 +248,7 @@ int _tmain(int argc, _TCHAR* argv[]){
 			sceneMan->InstantiateObject(sceneMan->findLayer("layer2"),12,x,y);
 			mousecounter = 0;
 		}
-		mousecounter++;*/
+		mousecounter++;
 		////////////////////////////////////
 
 		for (int i = 0; i < GameObjects.alive_objects.size(); i++){
