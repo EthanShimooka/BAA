4f01ce26dc760f82dd95f662ac162c45c0b8635f
gmmack <gmmack@ucsc.edu>
2016-04-02 17:08:11 -0700

    crosshair!
    circle for crosshair. not final version, going to use a bullseye image
    later but placeholder bullseye

--- Test/GameSession.cpp
+++ Test/GameSession.cpp
@@ -174,6 +174,8 @@ int GameSession::Run(){
 	Animation * runWater = new Animation(20, motions);
 	int aniCounter = 0;
 
+	SDL_Cursor* cursor = renderMan->cursorToCrosshair();
+
 	while (gameloop) {
 
 		std::cout << NetworkManager::sInstance->GetState() << std::endl;
@@ -246,6 +248,7 @@ int GameSession::Run(){
 	for (unsigned int i = 0; i < GameObjects.alive_objects.size(); i++){
 		GameObjects.DeleteObjects(GameObjects.alive_objects[i]->ID);
 	}
+	renderMan->freeCursor(cursor);
 	std::cout << renderMan << std::endl;
 
 	log->close();
