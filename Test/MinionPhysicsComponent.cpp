eb738bf61d19ad534b61c5b245cc7a401642bc04
gmmack <gmmack@ucsc.edu>
2016-04-09 21:45:09 -0700

    minion-base collision
    implemented the collision logic for minions and base colliding. It now
    destroys the minion and decreases base health. Still need to visually
    update the base to indicate dmg and trigger a shake effect for the
    attacked team

--- Test/MinionPhysicsComponent.cpp
+++ Test/MinionPhysicsComponent.cpp
@@ -32,24 +32,33 @@ void MinionPhysicsComponent::init(){
 	mBody->SetUserData(gameObjectRef);
 	mBody->SetTransform(b2Vec2(gameObjectRef->posX/worldScale, gameObjectRef->posY/worldScale), 0);
 	mBody->SetLinearVelocity(b2Vec2(50, 0));
-	setCollisionFilter(COLLISION_MINION, COLLISION_FEATHER | COLLISION_PLATFORM);
+	setCollisionFilter(COLLISION_MINION, COLLISION_FEATHER | COLLISION_PLATFORM | COLLISION_BASE);
 }
 
 void MinionPhysicsComponent::handleCollision(GameObject* otherObj){
 	//if hit, destroy minion or move it out of the alive_objects queue
 	//std::cout << "MINION handling collision with object ID: " << otherObj->ID << std::endl;
 	switch (otherObj->type){
-		case GAMEOBJECT_TYPE::OBJECT_FEATHER:
+	case GAMEOBJECT_TYPE::OBJECT_FEATHER:
 
-			gameObjectRef->setPos(-10000, 0);
-			//setCollisionFilter(COLLISION_MINION, 0);
-			gameObjectRef->isAlive = false;
-			//GameObjects.dead_feathers.push_back(gameObjectRef);
-			break;
+		gameObjectRef->setPos(-10000, 0);
+		//setCollisionFilter(COLLISION_MINION, 0);
+		gameObjectRef->isAlive = false;
+		//GameObjects.dead_feathers.push_back(gameObjectRef);
+		break;
 
-		case GAMEOBJECT_TYPE::OBJECT_MINION:
-			//just push each other around. Most likely done for us by box2d already
-			break;
+	case GAMEOBJECT_TYPE::OBJECT_MINION:
+		//just push each other around. Most likely done for us by box2d already
+		break;
+	case GAMEOBJECT_TYPE::OBJECT_BASE:{
+		//Still need to trigger shake effect, visually update dmg to base
+		//Currently destroys minions and updates base health logic
+		gameObjectRef->setPos(-10000, 0);
+		gameObjectRef->isAlive = false;
+		MidBaseLogicComponent* baseLogicComponent = dynamic_cast<MidBaseLogicComponent*>(gameObjectRef->GetComponent(COMPONENT_LOGIC));
+		baseLogicComponent->attacked();
+		break;
+	}
 		default:
 			break;
 	}
