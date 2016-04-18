9d4cc8802b6cba413f23d4273cc7e7b7baa2d923
gmmack <gmmack@ucsc.edu>
2016-04-18 15:13:55 -0700

    fixed screen_width/height for gameend

--- Test/GameEnd.cpp
+++ Test/GameEnd.cpp
@@ -13,8 +13,6 @@ void GameEnd::runGameEnd(){
 	RenderManager* renderMan = RenderManager::getRenderManager();
 	SceneManager* sceneMan = SceneManager::GetSceneManager();
 	std::string text;
-	int w, h;
-	renderMan->getWindowSize(&w, &h);
 	/*if (team won){
 	text = "Victory!";
 	}
@@ -22,7 +20,7 @@ void GameEnd::runGameEnd(){
 	text = "Defeat!";
 	}*/
 	text = "Victory!";
-	textHUD = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 7000, (float)3*w/7, (float)3*h/7, true);
+	textHUD = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 7000, (float)2*SCREEN_WIDTH/6, (float)3*SCREEN_HEIGHT/7, true);
 	textHUD->setResourceObject(renderMan->renderText(text.c_str(), 160, 32, 240, 70, "BowlbyOneSC-Regular"));
 	sceneMan->AssembleScene();
 	Sleep(3000);
