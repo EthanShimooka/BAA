827b07335333de9c1c410254b3198b1b136b1edf
gmmack <gmmack@ucsc.edu>
2016-04-18 15:04:11 -0700

    endgame position no longer hardcoded

--- Test/GameEnd.cpp
+++ Test/GameEnd.cpp
@@ -13,6 +13,8 @@ void GameEnd::runGameEnd(){
 	RenderManager* renderMan = RenderManager::getRenderManager();
 	SceneManager* sceneMan = SceneManager::GetSceneManager();
 	std::string text;
+	int w, h;
+	renderMan->getWindowSize(&w, &h);
 	/*if (team won){
 	text = "Victory!";
 	}
@@ -20,8 +22,7 @@ void GameEnd::runGameEnd(){
 	text = "Defeat!";
 	}*/
 	text = "Victory!";
-	//Position currently hard-coded, needs to be fixed once we have global screen width/height variables
-	textHUD = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 7000, 250, 300, true);
+	textHUD = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), 7000, (float)3*w/7, (float)3*h/7, true);
 	textHUD->setResourceObject(renderMan->renderText(text.c_str(), 160, 32, 240, 70, "BowlbyOneSC-Regular"));
 	sceneMan->AssembleScene();
 	Sleep(3000);
