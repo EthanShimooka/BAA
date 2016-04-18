28c15b5eca0aa67b2bb27e9644178fe3e4dfc68a
Ethan Shimooka <sshimook@ucsc.edu>
2016-04-18 16:00:31 -0700

    working on getting switces. switches get stiches
    Issues: collisions do not work with feathers

--- Test/MidPlatShieldLogicComponent.h
+++ Test/MidPlatShieldLogicComponent.h
@@ -1,4 +1,20 @@
+/**
+*  MidPlatShieldLogicComponent.h
+*  Authors: Ethan Shimooka
+*  Date 3/31/2016
+*  Description :
+Logic Component class for the midPlatform Shield
+
+*/
+
 #pragma once
+#ifndef MIDPLATSHIELDLOGICCOMPONENT_H_INCLUDED
+#define MIDPLATSHIELDLOGICCOMPONENT_H_INCLUDED
+
+#include "PhysicsComponent.h"
+#include "MidPlatShieldComponentIncludes.h"
+
+
 #include "LogicComponent.h"
 class MidPlatShieldLogicComponent :
 	public LogicComponent
@@ -8,3 +24,4 @@ public:
 	~MidPlatShieldLogicComponent();
 };
 
+#endif
