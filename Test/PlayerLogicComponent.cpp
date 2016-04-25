7aea423890e8efa31058d744f5bd2577871a3b5e
blakewilliams1 <blauwill@adgpm.soe.ucsc.edu>
2016-04-24 18:13:06 -0700

    test

--- Test/PlayerLogicComponent.cpp
+++ Test/PlayerLogicComponent.cpp
@@ -113,9 +113,10 @@ void PlayerLogicComponent::hatchBird(){
 				physicsComp->mBody->SetAngularVelocity(0);
 		}
 		else{
-			gameObjectRef->rotation = 0;
-			if (gameObjectRef->isLocal)physicsComp->mBody->SetTransform(pos, 0);
-			renderComp->allObjs["base"]->rotation = 0;
+			gameObjectRef->rotation = 180;
+			if (gameObjectRef->isLocal)physicsComp->mBody->SetTransform(pos, M_PI);
+			renderComp->allObjs["base"]->rotation = 180;
+			std::cout<<renderComp->objRef->getRotation() << std::endl;
 		}
 		isEgg = false;
 	}
