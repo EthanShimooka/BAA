27b60e48901e8d61a92eba1994abfca97d2bbdef
gmmack <gmmack@ucsc.edu>
2016-05-04 14:29:51 -0700

    cleaned up comments

--- Test/FanLogicComponent.cpp
+++ Test/FanLogicComponent.cpp
@@ -14,15 +14,13 @@ FanLogicComponent::~FanLogicComponent()
 
 void FanLogicComponent::Update(){
 	if (Timing::sInstance.EndPeacockAbilityTimer()){
-		std::cout << "INSIDE THE IF!!!!!" << std::endl;
 		PeacockClassComponent* peacockComp = dynamic_cast<PeacockClassComponent*>(GameObjects.GetGameObject(GamerServices::sInstance->GetLocalPlayerId())->GetComponent(COMPONENT_CLASS));
 		for (auto i = peacockComp->fanIDs.begin(); i != peacockComp->fanIDs.end(); i++){
-			std::cout << "BEFORE GETGAMEOBJCT CALL!" << std::endl;
-			GameObjects.GetGameObject(*i)->isAlive = false;
-			peacockComp->fanIDs.erase(i);
-			break;
-			std::cout << "AFTER GETGAMEOBJCT CALL!" << std::endl;
+			//if (isCorrectID){
+				GameObjects.GetGameObject(*i)->isAlive = false;
+				peacockComp->fanIDs.erase(i);
+				break;
+			//}
 		}
 	}
-	std::cout << "OUTSIDE THE IF!!!!!" << std::endl;
 }
