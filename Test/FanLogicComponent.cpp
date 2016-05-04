b7eb29ed957555a9cf9b2ee04f77fc606f67fb42
gmmack <gmmack@ucsc.edu>
2016-05-04 14:31:38 -0700

    added comment

--- Test/FanLogicComponent.cpp
+++ Test/FanLogicComponent.cpp
@@ -16,7 +16,7 @@ void FanLogicComponent::Update(){
 	if (Timing::sInstance.EndPeacockAbilityTimer()){
 		PeacockClassComponent* peacockComp = dynamic_cast<PeacockClassComponent*>(GameObjects.GetGameObject(GamerServices::sInstance->GetLocalPlayerId())->GetComponent(COMPONENT_CLASS));
 		for (auto i = peacockComp->fanIDs.begin(); i != peacockComp->fanIDs.end(); i++){
-			//if (isCorrectID){
+			//if (isCorrectID){//Need to find a way to check if it's the correct timer ending
 				GameObjects.GetGameObject(*i)->isAlive = false;
 				peacockComp->fanIDs.erase(i);
 				break;
