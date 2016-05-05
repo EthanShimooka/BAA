57f4105161c242ce83c1fdc2920e23c594f5b804
gmmack <gmmack@ucsc.edu>
2016-05-04 17:59:25 -0700

    no longer destroys all fans when multiple peacocks play
    It only destroys one fan. Still need to make it so that the timers
    aren't messed up if multiple peacock fans are thrown out.

--- Test/FanLogicComponent.cpp
+++ Test/FanLogicComponent.cpp
@@ -16,11 +16,9 @@ void FanLogicComponent::Update(){
 	if (Timing::sInstance.EndPeacockAbilityTimer()){
 		PeacockClassComponent* peacockComp = dynamic_cast<PeacockClassComponent*>(GameObjects.GetGameObject(GamerServices::sInstance->GetLocalPlayerId())->GetComponent(COMPONENT_CLASS));
 		for (auto i = peacockComp->fanIDs.begin(); i != peacockComp->fanIDs.end(); i++){
-			//if (isCorrectID){//Need to find a way to check if it's the correct timer ending
-				GameObjects.GetGameObject(*i)->isAlive = false;
-				peacockComp->fanIDs.erase(i);
-				break;
-			//}
+			GameObjects.GetGameObject(*i)->isAlive = false;
+			peacockComp->fanIDs.pop_front;
+			break;
 		}
 	}
 }
