e06b67d356d301a9b459fc385b28b550f15d01be
gmmack <gmmack@adgpm.soe.ucsc.edu>
2016-05-08 20:33:17 -0700

    test

--- Test/PeacockClassComponent.cpp
+++ Test/PeacockClassComponent.cpp
@@ -172,7 +172,7 @@ void PeacockClassComponent::animation(SDLRenderObject** objRef, map_obj& allObjs
 }
 
 int PeacockClassComponent::useAbility(){
-	if (currBirdseed >= seedRequired){
+	if (currBirdseed >= 1){
 		FanObjectFactory fFactory;
 		switch (activeFans){
 		case 0:
