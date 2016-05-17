b3d5d07da48aafb738ac3dc5b9be8d301a4f4ea7
Pourya Sohbati <sohbati.pourya@gmail.com>
2016-05-17 01:08:03 -0700

    added stats class but have to fix it

---
+++ Test/Stats.cpp
@@ -0,0 +1,22 @@
+#include "Stats.h"
+
+//Stats::Stats()
+//{
+//}
+//
+//
+//Stats::~Stats()
+//{
+//}
+//
+//void Stats::resetStats(){
+//	// Base health
+//	purple.baseHealthLost = 0;
+//	yellow.baseHealthLost = 0;
+//	// Minion killed
+//	purple.otherTeamMinionsKilled = 0;
+//	yellow.otherTeamMinionsKilled = 0;
+//	// Players killed
+//	purple.otherTeamPlayersKilled = 0;
+//	yellow.otherTeamPlayersKilled = 0;
+//}
