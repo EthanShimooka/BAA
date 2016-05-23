5b92dee50c9c1a45644e6dbcc7d331b6b75f3a19
Ethan <sshimook@ucsc.edu>
2016-05-23 14:38:46 -0700

    Added in new launcher rigging

--- Test/LauncherRenderComponent.cpp
+++ Test/LauncherRenderComponent.cpp
@@ -18,15 +18,16 @@ void LauncherRenderComponent::buildAnimation(){
 
 	objRef = sceneMan->InstantiateObject(sceneMan->findLayer("layer1"), -1, 0, 0);
 	//base->toggleIfRenderImage();
-	SDLRenderObject * launcher1 = sceneMan->InstantiateObject(sceneMan->findLayer("layer2"), 2026, 0, 600);
-	SDLRenderObject * launcher2 = sceneMan->InstantiateObject(sceneMan->findLayer("layer2"), 2027, 0, 600);
+	SDLRenderObject * launcher1 = sceneMan->InstantiateObject(sceneMan->findLayer("layer2"), 2026, -300, -600);
+	SDLRenderObject * launcher2 = sceneMan->InstantiateObject(sceneMan->findLayer("layer2"), 2027, -300, -600);
 
 
 	allObjs["base"] = objRef;
 	allObjs["launcher1"] = launcher1;
 	allObjs["launcher1"]->visible = true;
-
+	allObjs["launcher1"]->setScale(0.8f);
 	allObjs["launcher2"] = launcher2;
+	allObjs["launcher2"]->setScale(0.8f);
 	allObjs["launcher2"]->visible = false;
 
 
@@ -35,9 +36,13 @@ void LauncherRenderComponent::buildAnimation(){
 	launcher1->setAnchor(0.5f, 1.0f);
 	launcher2->setAnchor(0.5f, 1.0f);
 
+	////////////////////////////////////////////
+
 
 	std::list<motion> motions;
 	animations["idle"] = new Animation(100, motions);
+
+	////////////////////////////////////////////
 
 
 	std::list<motion> motions2;
@@ -50,6 +55,7 @@ void LauncherRenderComponent::buildAnimation(){
 	}
 	animations["throw"] = new Animation(400, motions2);
 	
+	////////////////////////////////////////////
 
 	std::list<motion> motions3;
 	if (gameObjectRef->team == TEAM_PURPLE){
@@ -57,40 +63,29 @@ void LauncherRenderComponent::buildAnimation(){
 
 	}
 	else{
-
-		motions3.push_back(makeMotion(moveLinearY(allObjs["launcher1"], 450, 0), 0, 1));
+		motions3.push_back(makeMotion(rotateTransform(allObjs["launcher1"], 20, -20), 0, 1));
+		motions3.push_back(makeMotion(moveLinearXY(allObjs["launcher1"], -450, -450, -250 , 100), 0, 1));
+		motions3.push_back(makeMotion(rotateTransform(allObjs["launcher2"], 20, -20), 0, 1));
+		motions3.push_back(makeMotion(moveLinearXY(allObjs["launcher2"], -450, -450, -250, 100), 0, 1));
 	}
+	animations["show"] = new Animation(200, motions3);
 
-
-	animations["show"] = new Animation(800, motions3);
+	///////////
 
 	std::list<motion> motions4;
 	if (gameObjectRef->team == TEAM_PURPLE){
-		motions4.push_back(makeMotion(moveLinearY(allObjs["launcher1"], 0, 450), 0, 1));
+		motions4.push_back(makeMotion(moveLinearY(allObjs["launcher1"], 0, -450), 0, 1));
 	}
 	else{
-
-		motions4.push_back(makeMotion(moveLinearY(allObjs["launcher1"], 0, 450), 0, 1));
+		motions4.push_back(makeMotion(rotateTransform(allObjs["launcher1"], -20, 20), 0, 1));
+		motions4.push_back(makeMotion(moveLinearXY(allObjs["launcher1"], -250, 100, -450, -450), 0, 1));
+		motions4.push_back(makeMotion(rotateTransform(allObjs["launcher2"], -20, 20), 0, 1));
+		motions4.push_back(makeMotion(moveLinearXY(allObjs["launcher2"], -250, 100, -450, -450), 0, 1));
 	}
 
-	animations["hide"] = new Animation(400, motions4);
+	animations["hide"] = new Animation(200, motions4);
 
-	std::list<motion> motions5;
-	animations["idle"] = new Animation(100, motions5);
-
-
-	if (gameObjectRef->team == TEAM_PURPLE){
-		motions5.push_back(makeMotion(rotateTransform(allObjs["launcher1"], 0, 0), 0, 1));
-	}
-	else{
-
-		motions5.push_back(makeMotion(rotateTransform(allObjs["launcher1"], 0, -10), 0, 1));
-	}
-	animations["windback"] = new Animation(400, motions5);
-
-
-
-
+	///////////
 }
 
 void LauncherRenderComponent::showLauncher(){
@@ -110,6 +105,9 @@ void LauncherRenderComponent::throwLauncher(){
 	setNextAnimation("hide");
 	setNextAnimation("idle");
 	std::cout << "SHIGGLE BUZZ BUZZ" << std::endl;
+
+	//allObjs["launcher1"]->visible = false;
+	//allObjs["launcher2"]->visible = true;
 
 }
 
