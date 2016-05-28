b1e966faed08261c29b6405f1e2414683f688488
PouryaSohbati <sohbati.pourya@gmail.com>
2016-05-27 20:00:49 -0700

    more stats functions

--- Test/Stats.cpp
+++ Test/Stats.cpp
@@ -45,3 +45,70 @@ void Stats::resetStats(){
 	// clear the map
 	allPlayersStats.clear();
 }
+
+void Stats::incBaseHealth(int team){
+	if (team == 1)
+		++yellow.baseHealth;
+	else if (team == 2)
+		++purple.baseHealth;
+}
+
+void Stats::incMinionsDied(int team){
+	if (team == 1)
+		++purple.otherTeamMinionsKilled;
+	else if (team == 2)
+		++yellow.otherTeamMinionsKilled;
+}
+
+void Stats::incPlayersDied(int team){
+	if (team == 1)
+		++purple.otherTeamPlayersKilled;
+	else if (team == 2)
+		++yellow.otherTeamPlayersKilled;
+}
+
+void Stats::incFeathersFired(int team){
+	if (team == 1)
+		++yellow.feathersFired;
+	else if (team == 2)
+		++purple.feathersFired;
+}
+
+void Stats::incAbilitiesUsed(int team){
+	if (team == 1)
+		++yellow.abilitiesUsed;
+	else if (team == 2)
+		++purple.abilitiesUsed;
+}
+
+int Stats::baseHealth(int team){
+	if (team == 1)
+		return yellow.baseHealth;
+	else if (team == 2)
+		return purple.baseHealth;
+	return -1;
+}
+
+int Stats::otherTeamMinionsKilled(int team){
+	if (team == 1)
+		return yellow.otherTeamMinionsKilled;
+	else if (team == 2)
+		return purple.otherTeamMinionsKilled;
+	return -1;
+}
+
+int Stats::otherTeamPlayersKilled(int team){
+	if (team == 1)
+		return yellow.otherTeamPlayersKilled;
+	else if (team == 2)
+		return purple.otherTeamPlayersKilled;
+	return -1;
+}
+
+int Stats::abilitiesUsed(int team){
+	if (team == 1)
+		return yellow.abilitiesUsed;
+	else if (team == 2)
+		return purple.abilitiesUsed;
+	return -1;
+}
