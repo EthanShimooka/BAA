07c4dca598775a426028aa68ffa371fae3d19f49
blakewilliams1 <blauwill@adgpm.soe.ucsc.edu>
2016-06-03 19:42:06 -0700

    I'm stupid

--- Test/BoomerangPhysicsComponent.cpp
+++ Test/BoomerangPhysicsComponent.cpp
@@ -47,7 +47,7 @@ void BoomerangPhysicsComponent::handleCollision(GameObject* otherObj){
 			PlayerRenderComponent* ownerRenderComp = dynamic_cast<PlayerRenderComponent*>(otherObj->GetComponent(COMPONENT_RENDER));
 			ownerRenderComp->allObjs["armL"]->visible = true;
 			ownerRenderComp->allObjs["armR"]->visible = true;
-			//gameObjectRef->isAlive = false;
+			gameObjectRef->isAlive = false;
 		}
 		break;
 	case  GAMEOBJECT_TYPE::OBJECT_MINION:
